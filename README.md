8f453e84f79e88ae852b7f2435d359d41d114e79
leandrosepu <sepu.fuenz@gmail.com>
2025-06-25 18:02:03 -0400

    change

--- README.md
+++ README.md
@@ -1,19 +1,2 @@
 #README
 #Instrucciones Taller
-
-\*Buscar Productos:
--> Por código
--> Por nombre
--> Precio Unitario
--> Cantidad disponible en moneda
--> Categoría del producto
--> Proovedor
-
-\*Agregar/Borrar Productos:
--> Por código
--> Por nombre
-
-\*Borrar Producto - Agregar Producto
-
-\*Ver todo el inventario - stock:
--> Nombre y Precio del Producto
--- main.py
+++ main.py
@@ -1,7 +1,45 @@
 #Taller 3 - Programacion
-# Benjamin Hidalgo - Franco Perez - Leandro Sepúlveda
+# Benjamin Hidalgo - Leandro Sepúlveda - Franco Perez
+
+import numpy as np
+
+listaNombres = [] #NombreProducto
+listaPrecios = [] #PrecioUnitario
+listaStocksAlmacenFerreteria = [] #StockAlmacenFerreteria
+listaCategorias = [] #,Categoria
+listaProveedores = [] #Proveedor
 
 #MAIN
 
+def lecturaArchivo():
+    
+    with open("ferreteria.txt", "r") as archivo:
+        linea = archivo.readline().strip()
+        while linea != "":
+            partes = linea.split(",")
+            nombre = partes[0]
+            precio = float(partes[1])
+            stockAlmacen = int(partes[2])
+            stockFerreteria = int(partes[3]) 
+            categoria = partes[4]
+            proveedor = partes[5]
+        
+            listaNombres.append(nombre)
+            listaPrecios.append(precio)
+            listaStocksAlmacenFerreteria.append(((stockAlmacen, stockFerreteria, nombre)))
+            listaCategorias.append(categoria)
+            listaProveedores.append(proveedor)
 
+            linea = archivo.readline().strip()
+        
+        archivo.close()
 
+columnas = len(listaNombres)
+filas = 2
+stocksM = np.zeros(filas, columnas)
+
+for stockAlmacen, stockFerreteria, nombre in listaStocksAlmacenFerreteria:
+    if nombre in listaNombres:
+        j = listaNombres.index(nombre)
+        stocksM[0][j] = stockAlmacen
+        stocksM[1][j] = stockFerreteria
